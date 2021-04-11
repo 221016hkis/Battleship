b6430159def8b19cded7caaf03ffcf516d02dfbe
Augustin Redon <augustin.redon@icloud.com>
2021-04-11 15:46:11 +0800

    Created the main class

--- BattleShip.java
+++ BattleShip.java
@@ -1,4 +1,20 @@
 import java.util.*;
+import java.util.Random;
 
 
+public class BattleShip {
 
+    static Maze myBattle = new Maze();
+    static Scanner input = new Scanner(System.in);
+
+    
+    public static void main(String[] args){
+        
+        myBattle.printMap();
+        myBattle.generateShips();
+    }
+
+
+}
+
+ 
